5327cf58f1a7e7d11dab8577fc26fe4ee5b788fd
Tejeswar Pavan Kalyan Saladi <saladipavankalyan10@gmail.com>
2025-09-20 10:20:30 +0000

    main module

---
+++ variables.tf
@@ -0,0 +1,41 @@
+variable "aws_region" {
+  description = "AWS region for resources"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "project_name" {
+  description = "Name of the project"
+  type        = string
+  default     = "devops-demo"
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "demo"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_app_subnet_cidrs" {
+  description = "CIDR blocks for private application subnets"
+  type        = list(string)
+  default     = ["10.0.10.0/24", "10.0.20.0/24"]
+}
+
+variable "private_db_subnet_cidrs" {
+  description = "CIDR blocks for private database subnets"
+  type        = list(string)
+  default     = ["10.0.100.0/24", "10.0.200.0/24"]
+}
